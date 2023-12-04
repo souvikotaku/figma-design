05ca59be38a13afea43025de3e99918a29be47db
souvikotaku <manson1989@>
2023-12-04 12:28:59 +0530

    bootstrap cdn added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App row">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="col-md-8">hello</div>
+      <div className="col-md-4">hello</div>
     </div>
   );
 }
